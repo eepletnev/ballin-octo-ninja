6fe0b02c2517fe6cdeecb37b139ff071072a6c2b
Evgeny Pletnev <ee.pletnev@me.com>
2014-04-07 11:24:35 +0400

    Final solution

--- wp-content/themes/twentyfourteen/thePage.php
+++ wp-content/themes/twentyfourteen/thePage.php
@@ -4,17 +4,30 @@
 Template Name: ballin-ninja
 */
 
-echo "uhjkhkjkhlhkkhlj";
-// The Query
-query_posts( $args );
 
-// The Loop
+query_posts( 'posts_per_page=8' );
+
+
+      $month = [];
+$monthAndDay = [];
+
+
 while ( have_posts() ) : the_post();
-    echo '<li>';
-    the_title();
-    echo '</li>';
+		array_push($month, get_the_time('M'));
+		array_push($monthAndDay, array(get_the_time('M'), get_the_time('d')));
 endwhile;
 
-// Reset Query
+$month = array_unique($month);
+
+
+
+	foreach ($month as $key => $val) {
+		echo $val . ": ";
+
+		foreach ($monthAndDay as $value) {
+			if ($val == $value[0]) echo $value[1] . " ";
+		}
+
+	}
 wp_reset_query();
 ?>
